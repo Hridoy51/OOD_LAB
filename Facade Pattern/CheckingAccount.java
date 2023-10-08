9c7b51daf20ae498078e720386cb5e18fd16ffb0
Moshiur Rahman <mrh6627@gmail.com>
2023-10-08 23:43:51 +0600

    all class are added and implementation is done

---
+++ Facade Pattern/CheckingAccount.java
@@ -0,0 +1,27 @@
+// CheckingAccount.java (Concrete Account Type)
+public class CheckingAccount implements Account {
+    private double balance;
+
+    public CheckingAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    @Override
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient funds");
+        }
+    }
+
+    @Override
+    public double getBalance() {
+        return balance;
+    }
+}
---
+++ Facade Pattern/SavingsAccount.java
@@ -0,0 +1,27 @@
+// SavingsAccount.java (Concrete Account Type)
+public class SavingsAccount implements Account {
+    private double balance;
+
+    public SavingsAccount(double initialBalance) {
+        this.balance = initialBalance;
+    }
+
+    @Override
+    public void deposit(double amount) {
+        balance += amount;
+    }
+
+    @Override
+    public void withdraw(double amount) {
+        if (balance >= amount) {
+            balance -= amount;
+        } else {
+            System.out.println("Insufficient funds");
+        }
+    }
+
+    @Override
+    public double getBalance() {
+        return balance;
+    }
+}
